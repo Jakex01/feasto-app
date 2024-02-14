b2bf8d406d6ee5f687457c4a1c66f68049d9c0d5
Jakex01 <131984341+Jakex01@users.noreply.github.com>
2024-02-14 10:17:29 +0100

    new commit to for adding components to my frontend

--- feasto-frontend/src/app/app.component.ts
+++ feasto-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'feasto-frontend';
+
+  prepareRoute(outlet: RouterOutlet): string | undefined {
+    return outlet.activatedRouteData['animation'] as string;
+  }
 }
